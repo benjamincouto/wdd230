b8f7a000cfe7a3de69d902986e129aef3245cc3c
benjamincouto <ben.couto@live.com>
2024-03-30 02:19:59 -0300

    checkpass

--- scripts/checkPassword.js
+++ scripts/checkPassword.js
@@ -6,7 +6,7 @@ password2.addEventListener("focusout", checkSame);
 
 function checkSame() {
   if (password1.value !== password2.value) {
-    message.textContent = "Password does not match";
+    message.textContent = "Password does not match. Please retry.";
     message.style.visibility = "show";
     password1.style.backgroundColor = "#fff0f3";
     password2.style.backgroundColor = "#fff0f3";
