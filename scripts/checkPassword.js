10890a5730620a20371ad057822f934a18789727
benjamincouto <ben.couto@live.com>
2024-03-30 02:16:46 -0300

    check password

--- scripts/checkPassword.js
+++ scripts/checkPassword.js
@@ -8,11 +8,13 @@ function checkSame() {
   if (password1.value !== password2.value) {
     message.textContent = "Password does not match";
     message.style.visibility = "show";
+    password1.style.backgroundColor = "#fff0f3";
     password2.style.backgroundColor = "#fff0f3";
     password2.value = "";
-    password2.focus();
+    password1.value = "";
+    password1.focus();
   } else {
-    message.style.display = "none";
+    message.textContent = "";
     password2.style.backgroundColor = "#fff";
     password2.style.color = "#000";
   }
