cfba4a0dd81cd23dc7306e03a2d6df81e6b5b544
benjamincouto <ben.couto@live.com>
2024-04-03 00:47:16 -0300

    removed console.log

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,13 @@
 const baseURL = "https://benjamincouto.github.io/wdd230/";
 const linksURL = "https://benjamincouto.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+  try {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getLinks();
